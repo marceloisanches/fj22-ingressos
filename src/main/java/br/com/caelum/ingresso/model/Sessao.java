6dd7b79714b88a3bb7be73d11e0426615a4dd87b
Marcelo Sanches <marcelo_ides@hotmail.com>
2018-07-07 16:15:38 -0300

    Implementando a seleção de lugares, ingressos e tipo de ingressos

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,13 +3,17 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao	{
@@ -25,14 +29,14 @@ public class Sessao	{
 	@ManyToOne
 	private	Filme filme;
 	
-	private	BigDecimal preco;
+	private BigDecimal preco = BigDecimal.ZERO;
 	
-	/**
-	*	@deprecated	hibernate	only
-	*/
-	public	Sessao()	{
+	public Sessao() {
 		
 	}
+	
+	@OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
 	
 	public	Sessao(LocalTime horario, Filme	filme, Sala	sala) {
 		this.horario = horario;
@@ -48,7 +52,11 @@ public class Sessao	{
 	public LocalTime getHorarioTermino() {
 		return	this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
-
+	
+	public boolean isDisponivel(Lugar lugarSelecionado) {
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+	
 	public Integer getId() {
 		return id;
 	}
--- src/main/java/br/com/caelum/ingresso/model/descontos/SemDesconto.java
+++ src/main/java/br/com/caelum/ingresso/model/descontos/SemDesconto.java
@@ -3,8 +3,15 @@ package br.com.caelum.ingresso.model.descontos;
 import java.math.BigDecimal;
 
 public class SemDesconto implements	Desconto {
+	
 	@Override
 	public BigDecimal aplicarDescontoSobre(BigDecimal precoOriginal)	{
-	return	precoOriginal;
+		return precoOriginal;
 	}
+	
+	@Override
+	public String getDescricao() {
+		return "Normal";
+	}
+
 }
